a84b6e1ea7f23164faf71fb0ca63090c8d4704b0
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-06-05 05:04:40 +0600

    add item added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const port = process.env.PORT || 5001;
 
@@ -32,7 +33,64 @@ async function run() {
         const scholarshipCollection = client.db("scholarshipManagement").collection("allScholarship");
         const scholarshipCartCollection = client.db("scholarshipManagement").collection("scholarshipCart");
 
+
+        // jwt related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '5h'
+            });
+            res.send({ token });
+        })
+        // middlewares
+        const verifyToken = (req, res, next) => {
+            // console.log('inside verify token', req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'unauthorized access' });
+            }
+
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'unauthorized access' });
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
+
+        // use verify admin after verifyToken
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+            next();
+        }
+
         // user related api
+        app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            let admin = false;
+            if (user) {
+                admin = user?.role === 'admin';
+            }
+            res.send({ admin });
+        })
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             // insert email if user doesn't exists:
@@ -40,15 +98,88 @@ async function run() {
             const query = { email: user.email }
             const existingUser = await userCollection.findOne(query);
             if (existingUser) {
-              return res.send({ message: 'user already exists', insertedId: null })
+                return res.send({ message: 'user already exists', insertedId: null })
             }
             const result = await userCollection.insertOne(user);
             res.send(result);
-          });
+        });
+
+        app.patch('/users/admin/:id', verifyAdmin, verifyToken, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+        })
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
 
         // all scholarship page
         app.get('/allScholarship', async (req, res) => {
             const result = await scholarshipCollection.find().toArray();
+            res.send(result);
+        })
+
+        // update page
+        app.get('/allScholarship/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await scholarshipCollection.findOne(query);
+            res.send(result);
+        })
+        // update item
+        app.patch('/allScholarship/:id', async (req, res) => {
+            const item = req.body;
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    scholarshipName: item.scholarshipName,
+                    universityName: item.universityName,
+                    universityCountry: item.universityCountry,
+                    universityCity: item.universityCity,
+                    subjectName: item.subjectName,
+                    subjectCategory: item.subjectCategory,
+                    scholarshipCategory: item.scholarshipCategory,
+                    degree: item.degree,
+                    universityWorldRank: item.universityWorldRank,
+                    rating: item.rating,
+                    stipend: item.stipend,
+                    applicationFees: item.applicationFees,
+                    serviceCharge: item.serviceCharge,
+                    tuitionFees: item.tuitionFees,
+                    applicationDeadline: item.applicationDeadline,
+                    scholarshipPostDate: item.scholarshipPostDate,
+                    scholarshipDetails: item.scholarshipDetails,
+                    image: item.image
+                }
+            }
+            const result = await scholarshipCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+        })
+
+        // add scholarship item
+        app.post('/allScholarship', verifyToken, verifyAdmin, async (req, res) => {
+            const item = req.body;
+            const result = await scholarshipCollection.insertOne(item);
+            res.send(result);
+        })
+
+        // delete scholarship item
+        app.delete('/allScholarship/:id', verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await scholarshipCollection.deleteOne(query);
             res.send(result);
         })
 
@@ -72,7 +203,7 @@ async function run() {
             const query = { _id: new ObjectId(id) }
             const result = await scholarshipCartCollection.deleteOne(query);
             res.send(result);
-          })
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
